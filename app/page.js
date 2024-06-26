2ce68cdd495fb6a44b10d929df292a1ece90c38d
Dexter829 <deepakk777829@gmail.com>
2024-06-27 01:00:19 +0530

    Implement root redirection to dashboard

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
-// This example assumes you're placing the middleware in the same directory as your page.js
-import { NextResponse } from 'next/server';
+import { Button } from "@/components/ui/button";
 
-export function middleware(request) {
-  // Check the path of the incoming request
-  if (request.nextUrl.pathname.startsWith('/app/page')) {
-    // Redirect to the dashboard
-    return NextResponse.redirect(new URL('/dashboard', request.url));
-  }
 
-  return NextResponse.next();
+export default function Home() {
+  return (
+    <div>
+      <h2>Hello</h2>
+      <Button>Hello Ji</Button>
+    </div>
+  );
 }
