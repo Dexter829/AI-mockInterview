1e65ccbcc9160a7834187e46630b2e21cfd76288
Dexter829 <deepakk777829@gmail.com>
2024-06-27 17:59:45 +0530

    Update page.jsx with new content and layout adjustments

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -5,11 +5,10 @@ export default function Page() {
     <section className="bg-white">
       <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
         <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
-          <img
-            alt=""
-            src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-            className="absolute inset-0 h-full w-full object-cover opacity-80"
-          />
+<img className="flex mx-auto max-h-screen w-auto object-cover object-center"
+  alt="Descriptive text for image"
+  src="/logo.jpg"
+/>
 
           <div className="hidden lg:relative lg:block lg:p-12">
             <a className="block text-white" href="#">
@@ -28,12 +27,11 @@ export default function Page() {
             </a>
 
             <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-              Welcome to Squid 🦑
+              Welcome to InterviewAce 
             </h2>
 
             <p className="mt-4 leading-relaxed text-white/90">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-              quibusdam aperiam voluptatum.
+            An AI based platform for the preparations of all sorts of Interviews.
             </p>
           </div>
         </section>
@@ -66,8 +64,7 @@ export default function Page() {
               </h1>
 
               <p className="mt-4 leading-relaxed text-gray-500">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-                quibusdam aperiam voluptatum.
+                An AI based platform for the preparations of all sorts of Interviews.
               </p>
             </div>
 
